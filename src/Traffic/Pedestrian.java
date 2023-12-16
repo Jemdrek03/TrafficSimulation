cfe237522d4618c317d9c10730635e55ccf9b7ea
jemdrek:} <j.radlowski03@gmail.com>
2023-12-16 03:53:58 +0100

    Plz work

--- src/Traffic/Pedestrian.java
+++ src/Traffic/Pedestrian.java
@@ -4,8 +4,8 @@ public class Pedestrian extends MovingObjects{
 
     private int pedestrianSize = 5;
 
-//    public Pedestrian(int x, int y, )
-//    {
-//        super()
-//    }
+    public Pedestrian(int x, int y, )
+    {
+        super()
+    }
 }
